b06f2fc2f9a3731a492e470fe39f0ffdaea64298
qiziyun <qiziyun@cmhi.chinamobile.com>
2022-05-13 11:33:28 +0800

    项目初始化

---
+++ src/main/java/com/qizy/MyHDFSApplication.java
@@ -0,0 +1,13 @@
+package com.qizy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHDFSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHDFSApplication.class, args);
+
+    }
+}
